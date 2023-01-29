be85f4e87bd85511a9633fdedd678ac2f669ea38
palomasoltys <pfs601@gmail.com>
2023-01-29 10:27:46 -0500

    Create Performance Test File for Missing Number

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MissingNumber {
     public static void main(String[] args) {
         System.out.println(missingNumber(new int[]{2, 1, 4, 3, 6, 5, 7, 10, 9}, 10)); // 8
@@ -9,7 +12,7 @@ public class MissingNumber {
 //    For example, given a list of numbers, in random order, of 1…10, 8 was removed. If this list and the maxNum of 10 was passed into the function,
 //    it would return 8: missingNumber({2, 1, 4, 3, 6, 5, 7, 10, 9}, 10) ⇒ 8
 //    If there is no missing number, the function should return 0.
-    static int missingNumber(int[] numbers, int maxNum) {
+    static int missingNumberSum(int[] numbers, int maxNum) {
 
         //RUN TIME: O(N)
         //SPACE COMPLEXITY: O(1)
@@ -23,5 +26,34 @@ public class MissingNumber {
             sum+=n;
         }
         return expectedSum - sum;
+    }
+
+    public static int missingNumber(int[] nums, int maxNum) {
+
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        for (int i = 1; i <= maxNum; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static int missingNumberSort(int[] nums, int maxNum) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < maxNum; i += 1) {
+            if (i != nums[i - 1]) {
+                return i;
+            }
+        }
+
+        // If none is missing
+        if (nums.length == maxNum && nums[maxNum - 1] == maxNum) {
+            return 0;
+        }
+
+        return maxNum;
     }
 }
